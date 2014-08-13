1b5445defb22dd4ce1f34eba46b28df878210bc9
Ray Villalobos <ray@lynda.com>
2014-08-13 02:05:52 -0400

    03_07

---
+++ components/scripts/script.js
@@ -0,0 +1,10 @@
+$(function() {
+  var wheight = $(window).height(); //get height of the window
+
+  $('.fullheight').css('height', wheight);
+
+  $(window).resize(function() {
+    var wheight = $(window).height(); //get height of the window
+    $('.fullheight').css('height', wheight);
+  })
+});
