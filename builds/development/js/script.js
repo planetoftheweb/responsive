452069ccb81ce44ee00eddc4c382b720136953cb
Ray Villalobos <ray@lynda.com>
2014-08-13 21:28:05 -0400

    Creating Tween Animation

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -29,8 +29,29 @@ $(function() {
   $(window).resize(function() {
     var wheight = $(window).height(); //get height of the window
     $('.fullheight').css('height', wheight);
-  })
-});
+  }) //on resize
+
+
+
+
+  //set up ScrollMagic
+
+  var controller = new ScrollMagic({
+    globalSceneOptions: {
+      triggerHook: "onLeave"
+    }
+  });
+
+  var attractionstween = TweenMax.staggerFromTo('#attractions article', 1, { opacity: 0, scale: 0 },
+      {delay: 1, opacity: 1, scale: 1,
+        ease: Back.easeOut});
+
+
+  var scene = new ScrollScene({
+    triggerElement: '#attractions'
+  }).setTween(attractionstween)
+    .addTo(controller);
+}); //on load
 }).call(this,typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"jquery":2}],2:[function(require,module,exports){
 /*!
--- components/scripts/script.js
+++ components/scripts/script.js
@@ -6,5 +6,26 @@ $(function() {
   $(window).resize(function() {
     var wheight = $(window).height(); //get height of the window
     $('.fullheight').css('height', wheight);
-  })
-});
+  }) //on resize
+
+
+
+
+  //set up ScrollMagic
+
+  var controller = new ScrollMagic({
+    globalSceneOptions: {
+      triggerHook: "onLeave"
+    }
+  });
+
+  var attractionstween = TweenMax.staggerFromTo('#attractions article', 1, { opacity: 0, scale: 0 },
+      {delay: 1, opacity: 1, scale: 1,
+        ease: Back.easeOut});
+
+
+  var scene = new ScrollScene({
+    triggerElement: '#attractions'
+  }).setTween(attractionstween)
+    .addTo(controller);
+}); //on load
