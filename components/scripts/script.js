c9eed4f2410e98eab2152f03fbee70280b94a3bf
Ahmed Chouihi <ahmedch1@users.noreply.github.com>
2020-05-20 22:35:22 +0100

    Update selector of href
    I have changed the selector of href as with this recent version of jquery , this line causes problem

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -16,7 +16,7 @@ $(function() {
 
 
 // Animated Scrolling
-  $('a[href*=#]:not([href=#])').click(function() {
+  $('a[href*=\\#]:not([href=\\#])').click(function() {
     if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
